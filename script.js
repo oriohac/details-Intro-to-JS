ac35df53c07ccc073df11b52d34a93f66e4cba73
Orioha Chikezirim <oriohac@gmail.com>
2022-06-22 17:22:50 +0100

    print my details using Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+const myName = "Chikezirim Obioma Orioha";
+let myHeight = 1.76;
+const myCountry = "Nigeria";
+
+document.writeln("My Name Is: "+myName);
+document.write("<br>")
+document.writeln("\nMy Height Is: "+myHeight+"m");
+document.write("<br>")
+document.writeln("\nMy Country Is: "+myCountry);
